06602d8a4da18c09e55c410fa6e55a6b8898ac82
Adrian Chiang <33133105+ajack2001my@users.noreply.github.com>
2018-01-07 02:23:28 +0800

    Add files via upload

--- tvlist.pas
+++ tvlist.pas
@@ -1,4 +1,5 @@
 {$DEFINE MASKLINKS}
+{$DEFINE ALTCOLORROWS}
 
 PROGRAM TVList;
 
@@ -11,18 +12,20 @@ USES
   SysUtils;
 
 CONST
-  ProgName             = 'TV List';
-  ProgVer              = 'v0.7';
-  ProgDate             = '20171125';
+  ProgName             = 'TVList';
+  ProgVer              = 'v0.8';
+  ProgDate             = '20180107';
   ProgAuthor           = 'Adrian Chiang';
-  ProgDesc             = 'An EZTV Series Manager';
+  ProgDesc             = 'A Torrent TV Series Manager';
 
   HTML_FontName        = 'Calibri';
+  
+  MyBitCoin            = '1FSMJRMk65o25frAsBoMYoZEZMkqncQ4Jm';
     
   CTime                = {$I %TIME%};
   CDate                = {$I %DATE%};
 
-  LL1 = '<I>$' + ProgName + '(';
+  LL1 = '$' + ProgName + '(';
   LL2 = ') took ';
   LL3 = ' second(s) to generate HTML...';
   
@@ -33,6 +36,7 @@ CONST
   Desc_TVNews     = 'News of shows, viewer feedback, ratings, etc.';
   Desc_Online     = 'Watch show online instead of downloading/torrenting.';
   Desc_Airs       = 'Day show airs, between seasons, mid-season break, ended or unknown.';
+  Desc_General    = 'Search in Google for your TV dramas.';
 
   URL_EZTV           = 'https://eztv.ag';
   URL_TPB            = 'https://thepiratebay.org';
@@ -44,6 +48,13 @@ CONST
   URL_ExtraTorrent   = 'https://extratorrent.ag';
   URL_LimeTorrent    = 'https://nntorrent.com';
   URL_MKVTV          = 'http://mkvtv.net';
+  URL_Openload       = 'http://openloadmovies.tv';
+  URL_Torz2          = 'https://torrentz2.eu';
+  URL_Leetx          = 'http://1337x.to';
+  URL_Google         = 'https://www.google.com';
+  URL_ReturnDates    = 'https://www.returndates.com';
+  
+  URL_TVList_Source  = 'https://www.github.com/ajack2001my/tvlist';
   
   Link_EZTV             = URL_EZTV;
   Link_EZTV_Search      = URL_EZTV + '/search/';
@@ -56,13 +67,24 @@ CONST
   Link_ExtraTorrent     = URL_ExtraTorrent + '/search/?search=';
   Link_LimeTorrent      = URL_LimeTorrent + '/search/all/';
   Link_MKVTV            = URL_MKVTV + '/?s=';
+  Link_Openload         = URL_Openload + '/?s=';
+  Link_Torz2            = URL_Torz2 + '/verified?f=';
+  Link_Leetx            = URL_Leetx + '/search/';
+  Link_Google           = URL_Google + '/search?&q=';
   
   TDStr = '<TD ALIGN="CENTER">';
 
   TVListString = 'tvlist';
   TVList_Data  = TVListString + '.txt';
   TVList_HTML  = TVListString + '.htm';
-
+  
+  Header_Color   = '"#FFFFFF"';
+  Header_BGColor = '"#000000"';
+  Link_Color     = '"#5F5F5F"';
+  VLink_Color    = '"#000000"';
+  Line1_BGColor  = '"#BBBBBB"';
+  Line2_BGColor  = '"#DDDDDD"';
+  
   TVListPNGSize = 279;
   TVListPNGName = TVListString + '.png';
   TVListPNG: Array [1..TVListPNGSize] of Byte = (137,  80,  78,  71,  13,  10,  26,  10,   0,   0, 
@@ -102,9 +124,13 @@ CONST
   VAR
   S,
   T         : Text;
+  LineCount,
   V,
   I         : Word;
   ShowDay,
+  L1,
+  L2,
+  L3,
   W,
   XN,
   XL        : String;
@@ -130,61 +156,87 @@ FUNCTION OSVersion: String;
 BEGIN
   OSVersion := 'Other';
   
-{$IFDEF LCLcarbon}
-  OSVersion := 'OS X';
-{$ENDIF}
-
-{$IFDEF Linux}
-  OSVersion := 'Linux';
-{$ENDIF}
-
 {$IFDEF UNIX}
   OSVersion := 'Unix';
+  {$IFDEF Linux}
+    OSVersion := 'Linux';
+  {$ENDIF}
 {$ENDIF}
 
 {$IFDEF MSWINDOWS}
   OSVersion := 'Windows';
+  {$IFDEF WIN32}
+     OSVersion := 'Win32';
+  {$ENDIF}	 
+  {$IFDEF WIN64}
+     OSVersion := 'Win64';
+  {$ENDIF}	 
 {$ENDIF}
 END;  
   
-PROCEDURE Write_Headers;
+PROCEDURE Write_HTML_Headers;
 BEGIN
+  WriteLn (T, '<!DOCTYPE HTML>'); 
   WriteLn (T, '<HTML><HEAD><TITLE>', ProgName, ' ', ProgVer, 
            '</TITLE><LINK REL="icon" TYPE="image/png" HREF="', TVListPNGName, '" /></HEAD>');
   Write   (T, '<BODY');
 
 {$IFDEF MASKLINKS}  
-  Write   (T, ' LINK="#808080" ALINK="#8080" VLINK="#000000"');
+  Write   (T, ' LINK=', Link_Color, ' ALINK=', VLink_Color, ' VLINK=', VLink_Color);
 {$ENDIF} 
  
   WriteLn (T, '>');
 END;
 
-PROCEDURE Write_Headers2;
+PROCEDURE Write_Headers;
 BEGIN
-  Write   (T, '<TABLE BORDER=1><TR>');
-  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Show, '">Show</DIV></TH>');
-  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_EZTV, '">EZTV</DIV></TH>');
-  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Search, '">Torrent Searches</DIV></TH>');
-  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_TVProfiles, '">TV Profiles</DIV></TH>');
-  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_TVNews, '">TV News</DIV></TH>');
-  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Online, '">Watch Online</DIV></TH>');
-  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Airs, '">Airs</DIV></TH>');
+  WriteLn (T, '<FONT FACE="', HTML_FontName, '">');
+  WriteLn (T, '<FONT SIZE=+1>', L1, '<BR>');
+  WriteLn (T, '&copy;' + L2 + '<P></FONT>');
+  WriteLn (T, '<FONT SIZE=-1><I>' + L3 + '</I><BR>');
+
+  Write   (T, '<TABLE WIDTH="100%" BORDER=1><TR BGCOLOR=', Header_BGColor, '>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Show, '"><FONT COLOR=', Header_Color, '>Show</FONT></DIV></TH>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Airs, '"><FONT COLOR=', Header_Color, '>Airs</FONT></DIV></TH>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_EZTV, '"><FONT COLOR=', Header_Color, '>EZTV</FONT></DIV></TH>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_General, '"><FONT COLOR=', Header_Color, '>General</FONT></DIV></TH>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Search, '"><FONT COLOR=', Header_Color, '>Torrent Searches</FONT></DIV></TH>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_TVProfiles, '"><FONT COLOR=', Header_Color, '>TV Profiles</FONT></DIV></TH>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_TVNews, '"><FONT COLOR=', Header_Color, '>TV News</FONT></DIV></TH>');
+  Write   (T, '<TH ALIGN="CENTER"><DIV TITLE="', Desc_Online, '"><FONT COLOR=', Header_Color, '>Watch Online</FONT></DIV></TH>');
   WriteLn (T, '</TR>');
 END;
 
 PROCEDURE Write_Bookmarks;
+  FUNCTION RepeatStr (S: String; N: Word):String;
+  VAR
+    I : Word;
+	X : String;
+  BEGIN
+    X := '';
+	FOR I := 1 TO N DO
+	  X := X + S;
+	RepeatStr := X;  
+  END;
 BEGIN
   Write (T, 'Homepages for <TT>');
 
   Write (T, '<A HREF="', URL_EZTV, '" TARGET="_BLANK">EZTV</A>,&nbsp;');
   Write (T, '<A HREF="', URL_TPB, '" TARGET="_BLANK">The Pirate Bay</A>,&nbsp;');
+  Write (T, '<A HREF="', URL_MKVTV, '" TARGET="_BLANK">MKVTV</A>,&nbsp;');
+  Write (T, '<A HREF="', URL_LimeTorrent, '" TARGET="_BLANK">Lime Torrent</A>,&nbsp;');
+  Write (T, '<A HREF="', URL_ExtraTorrent, '" TARGET="_BLANK">Extra Torrent</A>,&nbsp;');
+  Write (T, '<A HREF="', URL_Torz2, '" TARGET="_BLANK">Torrentz2</A>,&nbsp;');
+  Write (T, '<A HREF="', URL_Leetx, '" TARGET="_BLANK">1337x</A>,&nbsp;');
   Write (T, '<A HREF="', URL_TV, '" TARGET="_BLANK">TV</A>,&nbsp;');
+{
+  Write (T, '<BR>', RepeatStr ('&nbsp;', 13));
+}
   Write (T, '<A HREF="', URL_Variety, '" TARGET="_BLANK">Variety</A>,&nbsp;');
   Write (T, '<A HREF="', URL_TVLine, '" TARGET="_BLANK">TV Line</A>,&nbsp;');
   Write (T, '<A HREF="', URL_TVSeriesFinale, '" TARGET="_BLANK">TV Series Finale</A>,&nbsp;');
-  Write (T, '<A HREF="', URL_ExtraTorrent, '" TARGET="_BLANK">Extra Torrent</A>,&nbsp;');
-  Write (T, '<A HREF="', URL_LimeTorrent, '" TARGET="_BLANK">Lime Torrent</A>,&nbsp;');
+  Write (T, '<A HREF="', URL_Openload, '" TARGET="_BLANK">Openload</A>,&nbsp;');
+  Write (T, '<A HREF="', URL_ReturnDates, '" TARGET="_BLANK">Return Dates</A>,&nbsp;');
   
   Write (T, 'and&nbsp;');
   
@@ -195,12 +247,14 @@ END;
 
 PROCEDURE Write_Footer;
 BEGIN
-  WriteLn (T, '</TABLE><P/>');
+  WriteLn (T, '</TABLE><P>');
   Write_Bookmarks;
   xEnd := getTickCount64;
   xDiff := (xEnd - xStart) / 1000;
-  Write (T, '<P/><BR><FONT SIZE=-1>', LL1, OSVersion, LL2, xDiff:3:2, LL3);
-  Write (T, '<BR/>$Program compiled at ' + CTime + ' (local time) on ' + CDate + '</I>');
+  Write (T, '<P><I><BR><FONT SIZE=-1>', LL1, OSVersion, LL2, xDiff:3:2, LL3);
+  Write (T, '<BR/>$Program compiled at ' + CTime + ' (local time) on ' + CDate + '<BR>');
+  Write (T, '$Get the source code from <A HREF="', URL_TVList_Source, '" TARGET="_BLANK">github.com</A><BR>');
+  Write (T, '$BitCoin Donate&nbsp;<A HREF="bitcoin:', MyBitCoin, '">', MyBitCoin, '</A></I>');
   WriteLn (T, '</FONT></FONT>');
   WriteLn (T, '</BODY></HTML>');
 END;
@@ -236,10 +290,6 @@ BEGIN
 END;
 
 PROCEDURE Prog_Message;
-VAR
-  L1,
-  L2,
-  L3 : String;
 BEGIN
   L1 := ProgName + ' ' + ProgVer + ' - ' + ProgDesc + ' - Created by ' + ProgAuthor + '. Build ' + ProgDate;
   L2 := ' Copyright ' + ProgAuthor + ', 2015-20' + ProgDate[3] + ProgDate[4] + '. All Rights Reserved.';
@@ -251,11 +301,6 @@ BEGIN
   WriteLn ('(c)', L2);
   WriteLn;
   WriteLn (L3);
-
-  WriteLn (T, '<FONT FACE="', HTML_FontName, '">');
-  WriteLn (T, '<FONT SIZE=+1>', L1, '<BR>');
-  WriteLn (T, '&copy;' + L2 + '<P></FONT>');
-  WriteLn (T, '<FONT SIZE=-1><I>' + L3 + '</I><P></FONT>');
   WriteLn;
 END;
 
@@ -267,28 +312,27 @@ BEGIN
 END;
 
 
-FUNCTION RepSpace (C: Char; X: String): String;
+FUNCTION RepSpaceStr (C: Char; X: String): String;
 VAR
   I : Word;
 BEGIN
   FOR I := 1 TO Length(X) DO
     IF X[I] = ' ' THEN
       X[I] := C;
-  RepSpace := X;
+  RepSpaceStr := X;
 END;
 
 PROCEDURE ProcessFilesOpen;
 BEGIN
   IF NOT FileExists (TVList_Data) THEN
     RuntimeError ('Error, file "' + TVList_Data + '" not found.');
-	
   Assign (S, TVList_Data);
   Assign (T, TVList_HTML);
   Reset (S);
   ReWrite (T);
+  Write_HTML_Headers;
   Write_Headers;
-  Prog_Message;
-  Write_Headers2;
+  LineCount := 0;
 END;
 
 PROCEDURE ProcessData;
@@ -298,58 +342,77 @@ BEGIN
       REPEAT
         ReadLn (S, W);
       UNTIL (W[1] <> '#') OR Eof(S);
-  ShowDay := '<B>&#91;UNKWN&#93;</B>';
-  IF W[2] = '|' THEN
-    BEGIN
-      CASE UpCase(W[1]) OF
-        '0' : ShowDay := '<B>S------</B>';
-        '1' : ShowDay := '<B>-M-----</B>';
-        '2' : ShowDay := '<B>--T----</B>';
-        '3' : ShowDay := '<B>---W---</B>';
-        '4' : ShowDay := '<B>----T--</B>';
-        '5' : ShowDay := '<B>-----F-</B>';
-        '6' : ShowDay := '<B>------S</B>';
-        'Y' : ShowDay := '&#91;<B>BREAK</B>&#93;';
-        'Z' : ShowDay := '&#91;<B>ENDED</B>&#93;';
-      END;
-      Delete (W, 1, 2);
-    END;
-    V := Pos (',', W);
-    IF V > 1 THEN
-      BEGIN
-        XN := '';
-        FOR I := 1 TO (V - 1) DO
-          XN := XN + W[I];
-        Write (T, '<TR><TD>', XN, '</TD>');
-		
-        XL := '';
-        FOR I := (V + 1) TO Length (W) DO
-          XL := XL + W[I];
-        
-		Write (T, TDStr);
-		IF XL <> '0//' THEN
-          Write (T, '<A HREF="', Link_EZTV, '/shows/', XL, '" TARGET="_BLANK">Info</A>,&nbsp;');
-        Write (T, '<A HREF="', Link_EZTV_Search, RepSpace('-', XN), '" TARGET="_BLANK">Search</A></TD>');
+      ShowDay := '<B>&#91;UNKWN&#93;</B>';
+      IF W[2] = '|' THEN
+        BEGIN
+          CASE UpCase(W[1]) OF
+            '0' : ShowDay := '<B>S------</B>';
+            '1' : ShowDay := '<B>-M-----</B>';
+            '2' : ShowDay := '<B>--T----</B>';
+            '3' : ShowDay := '<B>---W---</B>';
+            '4' : ShowDay := '<B>----T--</B>';
+            '5' : ShowDay := '<B>-----F-</B>';
+            '6' : ShowDay := '<B>------S</B>';
+            'Y' : ShowDay := '<B>&#91;BREAK&#93;</B>';
+            'Z' : ShowDay := '<B>&#91;ENDED&#93;</B>';
+          END;
+          Delete (W, 1, 2);
+        END;
+      V := Pos (',', W);
+      IF V > 1 THEN
+        BEGIN
+          XN := '';
+          FOR I := 1 TO (V - 1) DO
+            XN := XN + W[I];
+          Write (T, '<TR');
 
-        Write (T, TDStr + '<A HREF="', Link_TPB, XN, '/" TARGET="_BLANK">PirateBay</A>,&nbsp;');
-        Write (T, '<A HREF="', Link_MKVTV, RepSpace('+', XN), '" TARGET="_BLANK">MkvTV</A>,&nbsp;');
-        Write (T, '<A HREF="', Link_LimeTorrent, XN, '/" TARGET="_BLANK">LimeTor</A>,&nbsp;');
-        Write (T, '<A HREF="', Link_ExtraTorrent, RepSpace('+', XN), '" TARGET="_BLANK">ExtraTor</A></TD>');
+{$IFDEF ALTCOLORROWS}
+		  IF (LineCount MOD 2) = 0 THEN
+		    Write (T, ' BGCOLOR=', Line1_BGColor)
+		  ELSE	
+		    Write (T, ' BGCOLOR=', Line2_BGColor);
+{$ENDIF}
+
+		  Write (T, '><TD>', XN, '</TD>');
 		
-        Write (T, TDStr + '<A HREF="', Link_TVcom, XN, '" TARGET="_BLANK">TVcom</A>,&nbsp;');
-        Write (T, '<A HREF="', Link_TVSF, RepSpace('-', XN), '" TARGET="_BLANK">TVfinale</A></TD>');
+          XL := '';
+          FOR I := (V + 1) TO Length (W) DO
+            XL := XL + W[I];
+
+
+          Write (T, TDStr + '<TT>', ShowDay, '</TT></TD>');
+        
+		  Write (T, TDStr);
+		  IF XL <> '0//' THEN
+            Write (T, '<A HREF="', Link_EZTV, '/shows/', XL, '" TARGET="_BLANK">Info</A>,&nbsp;');
+          
+		  Write (T, '<A HREF="', Link_EZTV_Search, RepSpaceStr('-', XN), '" TARGET="_BLANK">Search</A></TD>');
 		
-        Write (T, TDStr + '<A HREF="', Link_Variety_Search, XN, '" TARGET="_BLANK">Variety</A>,&nbsp;');
-        Write (T, '<A HREF="', Link_TVLine, RepSpace('-', XN), '" TARGET="_BLANK">TVLine</A></TD>');
+		  Write (T,  TDStr + '<A HREF="', Link_Google, RepSpaceStr('+', XN), '+%2BTV+%2BShow" TARGET="_BLANK">Google</A>');
 		
-        Write (T, TDStr + '<A HREF="', Link_YesMovies_Search, RepSpace('+', XN), '.html" TARGET="_BLANK">YesMovies</A></TD>');
-        Write (T, TDStr + '<TT>', ShowDay, '</TT></TD>');
-        WriteLn (T, '</TR>');
-        Write ('.');
-      END
-    ELSE
-      Write ('X');
-    END;
+          Write (T, TDStr + '<A HREF="', Link_TPB, XN, '/" TARGET="_BLANK">PirateBay</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_MKVTV, RepSpaceStr('+', XN), '" TARGET="_BLANK">MkvTV</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_LimeTorrent, XN, '/" TARGET="_BLANK">LimeTor</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_ExtraTorrent, RepSpaceStr('+', XN), '" TARGET="_BLANK">ExtraTor</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_Torz2, RepSpaceStr('+', XN), '&safe=1" TARGET="_BLANK">Torz2</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_Leetx, RepSpaceStr('+', XN), '/1/" TARGET="_BLANK">1337x</A></TD>');
+		
+          Write (T, TDStr + '<A HREF="', Link_TVcom, XN, '" TARGET="_BLANK">TVcom</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_TVSF, RepSpaceStr('-', XN), '" TARGET="_BLANK">TVfinale</A></TD>');
+		
+          Write (T, TDStr + '<A HREF="', Link_Variety_Search, XN, '" TARGET="_BLANK">Variety</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_TVLine, RepSpaceStr('-', XN), '" TARGET="_BLANK">TVLine</A></TD>');
+		
+          Write (T, TDStr + '<A HREF="', Link_YesMovies_Search, RepSpaceStr('+', XN), '.html" TARGET="_BLANK">YesMovies</A>,&nbsp;');
+          Write (T, '<A HREF="', Link_Openload, RepSpaceStr('+', XN), '" TARGET="_BLANK">Openload</A></TD>');
+
+          WriteLn (T, '</TR>');
+          Write ('.');
+          Inc (LineCount);
+        END
+      ELSE
+        Write ('X');
+	END;
   WriteLn (':done!');	
 END;
 
@@ -381,6 +444,7 @@ END;
   
 BEGIN
   GetDateTime;
+  Prog_Message;
   ProcessFilesOpen;
   CheckForProgIcon;
   ProcessData;
